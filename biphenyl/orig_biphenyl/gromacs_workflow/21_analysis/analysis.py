415fc8e4dd9a131ff194258843ebd727a590eab0
Lindsey Whitmore <lwhitm1@Lindseys-MacBook-Pro.local>
2024-02-20 20:37:55 -0700

    updating biphenyl

---
+++ biphenyl/orig_biphenyl/gromacs_workflow/21_analysis/analysis.py
@@ -0,0 +1,17 @@
+import alchemlyb.workflows
+import os
+import pandas as pd
+from alchemlyb.workflows import ABFE
+
+dir = "/projects/liwh2139/rotations/shirts/test/gromacs/new/molecules/biphenyl/gromacs_workflow/21_prod"
+
+outdirectory = '/projects/liwh2139/rotations/shirts/test/gromacs/new/molecules/biphenyl/gromacs_workflow/analysis/21_analysis'
+
+
+workflow = ABFE(units='kcal/mol', software='GROMACS', dir=dir,
+                prefix='dhdl', suffix='xvg', T=300, outdirectory=outdirectory)
+workflow.run(skiptime=100, uncorr='dhdl', threshold=50,
+             estimators=('MBAR', 'BAR', 'TI'), overlap='O_MBAR.pdf',
+             breakdown=True, forwrev=10)
+summary = workflow.generate_result()
+summary.to_csv('analysis.csv', index=False)
